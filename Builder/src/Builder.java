aa0131f1c8a9f16a216de649736f8d9ce0cacc4b
kerbey <kerbeychevalier@verizon.net>
2015-03-01 23:06:47 -0500

    Update Builder.java

--- Builder/src/Builder.java
+++ Builder/src/Builder.java
@@ -6,14 +6,14 @@ public class Builder
 		Scanner input=new Scanner(System.in);
 		System.out.println(" enter in a string");
 		String firstString=input.nextLine();
-		StringBuilder string= new StringBuilder();
-		StringBuilder g=string.append(firstString);
+		StringBuilder string= new StringBuilder();//String Builder class instance
+		StringBuilder g=string.append(firstString);//first string's length
 		System.out.println("this string's capacity is "+g.length());
 		String second=" I love it!";
 		System.out.println("enter in Yes,");
 		String Yes=input.nextLine();
 		
-		string.append(Yes);
+		string.append(Yes);//append yes string and secondString in order so they're printed in order
 		StringBuilder SecondString =string.append(second);
 		System.out.print(SecondString);
 		System.out.println();
